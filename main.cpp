d075e59940fade718b809986dba5eb52548c79af
Gabriel Schulhof <gabriel.schulhof@intel.com>
2019-12-09 10:43:17 -0800

    dump numbers received from OSX and add exception support

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <napi.h>
 #include <ApplicationServices/ApplicationServices.h>
 
@@ -5,10 +6,11 @@ Napi::Object getScreenSize(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
     Napi::Object obj = Napi::Object::New(env);
     CGDirectDisplayID displayID = CGMainDisplayID();
-    obj.Set("width",
-            Napi::Number::New(info.Env(), CGDisplayPixelsWide(displayID)));
-    obj.Set("height",
-            Napi::Number::New(info.Env(), CGDisplayPixelsHigh(displayID)));
+    uint32_t width = CGDisplayPixelsWide(displayID);
+    uint32_t height = CGDisplayPixelsHigh(displayID);
+    fprintf(stderr, "OSX returned %u x %u\n", width, height);
+    obj.Set("width", Napi::Number::New(info.Env(), width));
+    obj.Set("height", Napi::Number::New(info.Env(), height));
     return obj;
 }
 
